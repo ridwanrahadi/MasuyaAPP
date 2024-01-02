941ca3feab6ca260a3dd0aebe4bbebae3e1cd4f6
ridwanrahadi <90244050+ridwanrahadi@users.noreply.github.com>
2024-01-02 14:01:23 +0700

    5.7.0.0

--- Master Form/FrmLaporan.Designer.vb
+++ Master Form/FrmLaporan.Designer.vb
@@ -56,17 +56,19 @@ Partial Class FrmLaporan
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(13, 83)
+        Me.Label4.Location = New System.Drawing.Point(17, 102)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(76, 16)
+        Me.Label4.Size = New System.Drawing.Size(91, 20)
         Me.Label4.TabIndex = 6
         Me.Label4.Text = "Data Table"
         '
         'BtKeluar
         '
-        Me.BtKeluar.Location = New System.Drawing.Point(984, 41)
+        Me.BtKeluar.Location = New System.Drawing.Point(1312, 50)
+        Me.BtKeluar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtKeluar.Name = "BtKeluar"
-        Me.BtKeluar.Size = New System.Drawing.Size(75, 23)
+        Me.BtKeluar.Size = New System.Drawing.Size(100, 28)
         Me.BtKeluar.TabIndex = 8
         Me.BtKeluar.Text = "Exit"
         Me.BtKeluar.UseVisualStyleBackColor = True
@@ -94,58 +96,65 @@ Partial Class FrmLaporan
         DataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText
         DataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
         Me.DGLaporan.DefaultCellStyle = DataGridViewCellStyle4
-        Me.DGLaporan.Location = New System.Drawing.Point(15, 102)
+        Me.DGLaporan.Location = New System.Drawing.Point(20, 126)
+        Me.DGLaporan.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.DGLaporan.Name = "DGLaporan"
         Me.DGLaporan.ReadOnly = True
-        Me.DGLaporan.Size = New System.Drawing.Size(1044, 331)
+        Me.DGLaporan.Size = New System.Drawing.Size(1392, 407)
         Me.DGLaporan.TabIndex = 9
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(660, 442)
+        Me.Label5.Location = New System.Drawing.Point(880, 544)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(106, 13)
+        Me.Label5.Size = New System.Drawing.Size(138, 17)
         Me.Label5.TabIndex = 12
         Me.Label5.Text = "Total Include Rupiah"
         '
         'Totaltxt
         '
-        Me.Totaltxt.Location = New System.Drawing.Point(772, 439)
+        Me.Totaltxt.Location = New System.Drawing.Point(1029, 540)
+        Me.Totaltxt.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Totaltxt.Name = "Totaltxt"
-        Me.Totaltxt.Size = New System.Drawing.Size(140, 20)
+        Me.Totaltxt.Size = New System.Drawing.Size(185, 22)
         Me.Totaltxt.TabIndex = 13
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(918, 442)
+        Me.Label6.Location = New System.Drawing.Point(1224, 544)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(50, 13)
+        Me.Label6.Size = New System.Drawing.Size(66, 17)
         Me.Label6.TabIndex = 11
         Me.Label6.Text = "Total Qty"
         '
         'TotalQtytxt
         '
-        Me.TotalQtytxt.Location = New System.Drawing.Point(974, 439)
+        Me.TotalQtytxt.Location = New System.Drawing.Point(1299, 540)
+        Me.TotalQtytxt.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TotalQtytxt.Name = "TotalQtytxt"
-        Me.TotalQtytxt.Size = New System.Drawing.Size(85, 20)
+        Me.TotalQtytxt.Size = New System.Drawing.Size(112, 22)
         Me.TotalQtytxt.TabIndex = 14
         '
         'BtStock
         '
-        Me.BtStock.Location = New System.Drawing.Point(903, 41)
+        Me.BtStock.Location = New System.Drawing.Point(1204, 50)
+        Me.BtStock.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtStock.Name = "BtStock"
-        Me.BtStock.Size = New System.Drawing.Size(75, 23)
+        Me.BtStock.Size = New System.Drawing.Size(100, 28)
         Me.BtStock.TabIndex = 15
         Me.BtStock.Text = "Stock"
         Me.BtStock.UseVisualStyleBackColor = True
         '
         'BtExport
         '
-        Me.BtExport.Location = New System.Drawing.Point(822, 41)
+        Me.BtExport.Location = New System.Drawing.Point(1096, 50)
+        Me.BtExport.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtExport.Name = "BtExport"
-        Me.BtExport.Size = New System.Drawing.Size(75, 23)
+        Me.BtExport.Size = New System.Drawing.Size(100, 28)
         Me.BtExport.TabIndex = 16
         Me.BtExport.Text = "Export"
         Me.BtExport.UseVisualStyleBackColor = True
@@ -153,44 +162,49 @@ Partial Class FrmLaporan
         'CBType
         '
         Me.CBType.FormattingEnabled = True
-        Me.CBType.Location = New System.Drawing.Point(854, 75)
+        Me.CBType.Location = New System.Drawing.Point(1139, 92)
+        Me.CBType.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CBType.MaxLength = 40
         Me.CBType.Name = "CBType"
-        Me.CBType.Size = New System.Drawing.Size(205, 21)
+        Me.CBType.Size = New System.Drawing.Size(272, 24)
         Me.CBType.TabIndex = 24
         '
         'TglAkhir
         '
         Me.TglAkhir.CustomFormat = "M/dd/yyyy"
         Me.TglAkhir.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.TglAkhir.Location = New System.Drawing.Point(854, 15)
+        Me.TglAkhir.Location = New System.Drawing.Point(1139, 18)
+        Me.TglAkhir.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TglAkhir.Name = "TglAkhir"
-        Me.TglAkhir.Size = New System.Drawing.Size(124, 20)
+        Me.TglAkhir.Size = New System.Drawing.Size(164, 22)
         Me.TglAkhir.TabIndex = 22
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(780, 78)
+        Me.Label1.Location = New System.Drawing.Point(1040, 96)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.Size = New System.Drawing.Size(90, 17)
         Me.Label1.TabIndex = 18
         Me.Label1.Text = "Type Barang"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(637, 21)
+        Me.Label2.Location = New System.Drawing.Point(849, 26)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.Size = New System.Drawing.Size(60, 17)
         Me.Label2.TabIndex = 19
         Me.Label2.Text = "Tanggal"
         '
         'BtCari
         '
-        Me.BtCari.Location = New System.Drawing.Point(984, 12)
+        Me.BtCari.Location = New System.Drawing.Point(1312, 15)
+        Me.BtCari.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtCari.Name = "BtCari"
-        Me.BtCari.Size = New System.Drawing.Size(75, 23)
+        Me.BtCari.Size = New System.Drawing.Size(100, 28)
         Me.BtCari.TabIndex = 23
         Me.BtCari.Text = "Proses"
         Me.BtCari.UseVisualStyleBackColor = True
@@ -198,9 +212,10 @@ Partial Class FrmLaporan
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(819, 21)
+        Me.Label3.Location = New System.Drawing.Point(1092, 26)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.Size = New System.Drawing.Size(35, 17)
         Me.Label3.TabIndex = 20
         Me.Label3.Text = "s / d"
         '
@@ -208,25 +223,28 @@ Partial Class FrmLaporan
         '
         Me.TglAwal.CustomFormat = "M/dd/yyyy"
         Me.TglAwal.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.TglAwal.Location = New System.Drawing.Point(689, 15)
+        Me.TglAwal.Location = New System.Drawing.Point(919, 18)
+        Me.TglAwal.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TglAwal.Name = "TglAwal"
-        Me.TglAwal.Size = New System.Drawing.Size(121, 20)
+        Me.TglAwal.Size = New System.Drawing.Size(160, 22)
         Me.TglAwal.TabIndex = 21
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(716, 41)
+        Me.Button1.Location = New System.Drawing.Point(955, 50)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.Size = New System.Drawing.Size(133, 28)
         Me.Button1.TabIndex = 25
         Me.Button1.Text = "Keterangan JL"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'BTGp
         '
-        Me.BTGp.Location = New System.Drawing.Point(635, 41)
+        Me.BTGp.Location = New System.Drawing.Point(847, 50)
+        Me.BTGp.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BTGp.Name = "BTGp"
-        Me.BTGp.Size = New System.Drawing.Size(75, 23)
+        Me.BTGp.Size = New System.Drawing.Size(100, 28)
         Me.BTGp.TabIndex = 26
         Me.BTGp.Text = "Analisa GP"
         Me.BTGp.UseVisualStyleBackColor = True
@@ -234,9 +252,10 @@ Partial Class FrmLaporan
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(610, 78)
+        Me.Label7.Location = New System.Drawing.Point(813, 96)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(61, 13)
+        Me.Label7.Size = New System.Drawing.Size(80, 17)
         Me.Label7.TabIndex = 27
         Me.Label7.Text = "Divisi Sales"
         '
@@ -245,17 +264,19 @@ Partial Class FrmLaporan
         Me.CBSales.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.CBSales.FormattingEnabled = True
         Me.CBSales.Items.AddRange(New Object() {"FOOD", "MODERN", "LOCAL"})
-        Me.CBSales.Location = New System.Drawing.Point(677, 75)
+        Me.CBSales.Location = New System.Drawing.Point(903, 92)
+        Me.CBSales.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CBSales.Name = "CBSales"
-        Me.CBSales.Size = New System.Drawing.Size(97, 21)
+        Me.CBSales.Size = New System.Drawing.Size(128, 24)
         Me.CBSales.TabIndex = 28
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(373, 78)
+        Me.Label8.Location = New System.Drawing.Point(497, 96)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(77, 13)
+        Me.Label8.Size = New System.Drawing.Size(101, 17)
         Me.Label8.TabIndex = 29
         Me.Label8.Text = "Sales Wilayah "
         '
@@ -264,17 +285,19 @@ Partial Class FrmLaporan
         Me.CmbWil.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.CmbWil.FormattingEnabled = True
         Me.CmbWil.Items.AddRange(New Object() {"BANDUNG", "CIREBON", "LUAR KOTA"})
-        Me.CmbWil.Location = New System.Drawing.Point(456, 75)
+        Me.CmbWil.Location = New System.Drawing.Point(608, 92)
+        Me.CmbWil.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CmbWil.Name = "CmbWil"
-        Me.CmbWil.Size = New System.Drawing.Size(148, 21)
+        Me.CmbWil.Size = New System.Drawing.Size(196, 24)
         Me.CmbWil.TabIndex = 30
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(13, 22)
+        Me.Label9.Location = New System.Drawing.Point(17, 27)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(45, 13)
+        Me.Label9.Size = New System.Drawing.Size(58, 17)
         Me.Label9.TabIndex = 31
         Me.Label9.Text = "Wilayah"
         '
@@ -284,26 +307,28 @@ Partial Class FrmLaporan
         Me.CBKota.Enabled = False
         Me.CBKota.FormattingEnabled = True
         Me.CBKota.Items.AddRange(New Object() {"BANDUNG", "CIREBON"})
-        Me.CBKota.Location = New System.Drawing.Point(64, 21)
+        Me.CBKota.Location = New System.Drawing.Point(85, 26)
+        Me.CBKota.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CBKota.Name = "CBKota"
-        Me.CBKota.Size = New System.Drawing.Size(234, 21)
+        Me.CBKota.Size = New System.Drawing.Size(352, 24)
         Me.CBKota.TabIndex = 32
         '
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(304, 21)
+        Me.CheckBox1.Location = New System.Drawing.Point(445, 26)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(89, 17)
+        Me.CheckBox1.Size = New System.Drawing.Size(115, 21)
         Me.CheckBox1.TabIndex = 33
         Me.CheckBox1.Text = "Filter Wilayah"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'FrmLaporan
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1071, 466)
+        Me.ClientSize = New System.Drawing.Size(1428, 574)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.CBKota)
         Me.Controls.Add(Me.Label9)
@@ -330,6 +355,7 @@ Partial Class FrmLaporan
         Me.Controls.Add(Me.BtKeluar)
         Me.Controls.Add(Me.Label4)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.MaximizeBox = False
         Me.Name = "FrmLaporan"
         Me.ShowIcon = False
